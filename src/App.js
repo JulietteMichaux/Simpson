f29af612c4417159c07325a961e8311ab6bec541
Juliette Michaux <miamiame@hotmail.com>
2019-03-27 13:48:50 +0100

    Logo changed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+import Quotes from "./Quotes";
+import Lamp from "./Lamp";
+
+      
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: true,
+      
+    };
+  }
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+  };
   render() {
     return (
+      <div>
+      <header className="App-header">
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+        <img src={logo} className={this.state.working ? "App-logo":"Logo-modif"} alt="logo" />
+        <h1 className="App-title">Simpsons Quotes</h1>
+        <button onClick={this.handleClick}>Bouton</button>
+        </div>
+      </header>
+       </div>
+    )
   }
 }
 
